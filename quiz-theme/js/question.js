3b248629676c0b504be59fd271e81b8c9c6e2eae
thangdinh95 <tdxxi95@gmail.com>
2017-08-08 15:13:42 -1200

    change theme

---
+++ quiz-theme/js/question.js
@@ -0,0 +1,31 @@
+var questions = [{
+	"question" : "Who is a singer?",
+	"option1" : "Adele",
+	"option2" : "Gal Gadot",
+	"option3" : "Brad Pitt",
+	"answer" : "Adele",
+}, {
+	"question" : "Where is the capital city of UK?",
+	"option1" : "Manchester",
+	"option2" : "Seattle",
+	"option3" : "London",
+	"answer" : "London",
+}, {
+	"question" : "Traditional food in Vietnam is...?",
+	"option1" : "Pizza",
+	"option2" : "Pho",
+	"option3" : "Nem chua ran",
+	"answer" : "Pho",
+}, {
+	"question" : "Who is the leader of 2NE1?",
+	"option1" : "BOM",
+	"option2" : "CL",
+	"option3" : "MINZY",
+	"answer" : "CL",
+}, {
+	"question" : "Eiffel Tower is a popular place of...?",
+	"option1" : "France",
+	"option2" : "Germany",
+	"option3" : "USA",
+	"answer" : "France",
+}];
