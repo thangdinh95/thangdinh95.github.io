7fdf3a6c268d378ffa1438fc2c3bdfb665e6ce78
thangdinh95 <tdxxi95@gmail.com>
2017-11-16 16:35:12 -1200

    modalboxupdate

---
+++ modalbox/js/style.js
@@ -0,0 +1,108 @@
+$(function () {
+
+	//hiện modal ra
+	$('.open1').on('click',function(event) {
+		event.preventDefault();
+		$('.modal1').addClass('show');
+		$('.grey').addClass('show');
+	});
+
+	//đóng modal lại
+	$('.close').on('click',function(event) {
+		event.preventDefault();
+		$('.modal1').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+	$('.grey').on('click',function(event) {
+		event.preventDefault();
+		$('.modal1').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+//------------------------------------------------
+
+	$('.open2').on('click',function(event) {
+		event.preventDefault();
+		$('.modal2').addClass('show');
+		$('.grey').addClass('show');
+	});
+
+	//đóng modal lại
+	$('.close').on('click',function(event) {
+		event.preventDefault();
+		$('.modal2').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+	$('.grey').on('click',function(event) {
+		event.preventDefault();
+		$('.modal2').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+//------------------------------------------------
+
+	$('.open3').on('click',function(event) {
+		event.preventDefault();
+		$('.modal3').addClass('show');
+		$('.grey').addClass('show');
+	});
+
+	//đóng modal lại
+	$('.close').on('click',function(event) {
+		event.preventDefault();
+		$('.modal3').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+	$('.grey').on('click',function(event) {
+		event.preventDefault();
+		$('.modal3').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+//------------------------------------------------
+
+	$('.open4').on('click',function(event) {
+		event.preventDefault();
+		$('.modal4').addClass('show');
+		$('.grey').addClass('show');
+	});
+
+	//đóng modal lại
+	$('.close').on('click',function(event) {
+		event.preventDefault();
+		$('.modal4').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+	$('.grey').on('click',function(event) {
+		event.preventDefault();
+		$('.modal4').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+
+//------------------------------------------------
+
+	$('.open5').on('click',function(event) {
+		event.preventDefault();
+		$('.modal5').addClass('show');
+		$('.grey').addClass('show');
+	});
+
+	//đóng modal lại
+	$('.close').on('click',function(event) {
+		event.preventDefault();
+		$('.modal5').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+	$('.grey').on('click',function(event) {
+		event.preventDefault();
+		$('.modal5').removeClass('show');
+		$('.grey').removeClass('show');
+	});
+
+});
