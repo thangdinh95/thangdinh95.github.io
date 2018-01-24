28e42f853f9a5aee6922142f3f2820809c00c584
thang <tdxxi95@gmail.com>
2018-01-24 15:06:21 +0700

    del

--- Greenshop/js/slide.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-  $('.slide-product').slick({
-    prevArrow: '<div class="slick-left"><i class="fa fa-angle-left"></i><span class="sr-only">Prev</span></div>',
-    nextArrow: '<div class="slick-right"><i class="fa fa-angle-right"></i><span class="sr-only">Next</span></div>'
-  });
-
-});
-	
